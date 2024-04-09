16a21ec5483479d6cd63c37c3e9d2a721e936cf0
geoteodorescu <teodorescu.george@yahoo.com>
2024-04-09 21:12:40 +0300

    Initial application

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿//
+class Program
+{
+    static void Main()
+    {
+        Fn_ex4();
+    }
+    static int GetMin(int nr1, int nr2)
+    {
+        if (nr1 > nr2)
+        {
+            return nr2;
+        }
+        else if (nr1 < nr2)
+        {
+            return nr1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    static void Fn_ex4()
+    {
+        Console.WriteLine("Scrie primul numar:");
+        var firstNumber = Console.ReadLine();
+        Console.WriteLine("Scrie al doilea numar:");
+        var secondNumber = Console.ReadLine();
+        bool success1 = int.TryParse(firstNumber, out int result_1);
+        bool success2 = int.TryParse(secondNumber, out int result_2);
+        Console.WriteLine("Numarul cel mai mic este: " + GetMin(result_1, result_2));
+
+        /*
+        bool success1 = int.TryParse(firstNumber, out int result_1);
+        Console.WriteLine("Scrie al doilea numar:");
+        var secondNumber = Console.ReadLine();
+        bool success2 = int.TryParse(secondNumber, out int result_2);
+        if (success1 == true && success2 == true)
+        {
+            Console.WriteLine("Numarul cel mai mic este: " + GetMin(result_1, result_2));
+        }
+        else
+        {
+            Console.WriteLine("Ups.");
+        }
+        */
+    }
+}

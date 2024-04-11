d6a10bbd8badf1ea7f2df423e4a3c5cabd496c77
geoteodorescu <teodorescu.george@yahoo.com>
2024-04-11 19:00:04 +0300

    new upload strings course

---
+++ ConsoleApp2-Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class Program
+{
+    public static void Main()
+    {
+        string myFirstString = "Hello world!";
+        Console.WriteLine(myFirstString);
+
+        int myNumber = 44;
+        string myNumberAsString = myNumber.ToString();
+
+
+
+
+
+    }
+}
